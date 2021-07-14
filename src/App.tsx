aa5447a7c85fdd1a364cd48f84b95699ee8ca261
Aleix Riba <aleixriba33@gmail.com>
2021-07-14 21:47:43 +0200

    configure eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import 'tailwindcss/tailwind.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<div className='App'>
+			<h1>Hola</h1>
+		</div>
+	)
 }
 
-export default App;
+export default App
