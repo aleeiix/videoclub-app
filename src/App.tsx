10cc89fcbb269ad08788c0ef473ba8ca4d1364aa
Aleix Riba <aleixriba33@gmail.com>
2021-07-16 23:12:39 +0200

    add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-// import 'tailwindcss/tailwind.css'
+import { Button } from '@material-ui/core'
 
 const App = () => {
 	return (
 		<div className='App'>
 			<h1>Hola</h1>
+			<Button variant='contained' color='primary'>
+				Hola Mundo!
+			</Button>
 		</div>
 	)
 }
